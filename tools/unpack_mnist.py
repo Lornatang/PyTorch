6094204596d1d1214e3d5fe71756dff4cffb3042
Lornatang <shiyipaisizuo@163.com>
2019-01-26 01:09:27 +0800

    Create unpack_mnist.py

---
+++ tools/unpack_mnist.py
@@ -0,0 +1,48 @@
+import numpy as np
+import struct
+
+from PIL import Image
+import os
+
+data_file = '../../../../data/MNIST/mnist/t10k-images-idx3-ubyte'  # 需要修改的路径
+# It's 47040016B, but we should set to 47040000B
+data_file_size = 7840016
+data_file_size = str(data_file_size - 16) + 'B'
+
+data_buf = open(data_file, 'rb').read()
+
+magic, numImages, numRows, numColumns = struct.unpack_from(
+    '>IIII', data_buf, 0)
+datas = struct.unpack_from(
+    '>' + data_file_size, data_buf, struct.calcsize('>IIII'))
+datas = np.array(datas).astype(np.uint8).reshape(
+    numImages, 1, numRows, numColumns)
+
+label_file = '../../../../data/MNIST/mnist/t10k-labels-idx1-ubyte'  # 需要修改的路径
+
+# It's 60008B, but we should set to 60000B
+label_file_size = 10008
+label_file_size = str(label_file_size - 8) + 'B'
+
+label_buf = open(label_file, 'rb').read()
+
+magic, numLabels = struct.unpack_from('>II', label_buf, 0)
+labels = struct.unpack_from(
+    '>' + label_file_size, label_buf, struct.calcsize('>II'))
+labels = np.array(labels).astype(np.int64)
+
+datas_root = 'val'  # 需要修改的路径
+if not os.path.exists(datas_root):
+    os.mkdir(datas_root)
+
+for i in range(10):
+    file_name = datas_root + os.sep + str(i)
+    if not os.path.exists(file_name):
+        os.mkdir(file_name)
+
+for ii in range(numLabels):
+    img = Image.fromarray(datas[ii, 0, 0:28, 0:28])
+    label = labels[ii]
+    file_name = datas_root + os.sep + str(label) + os.sep + \
+                'mnist_train_' + str(ii) + '.png'
+    img.save(file_name)
