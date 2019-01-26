8999ff771c2728e74de58e1d5bdca77bac351c5f
Lornatang <shiyipaisizuo@163.com>
2019-01-26 12:37:14 +0800

    init

---
+++ research/PASCAL/P2008/train.py
@@ -0,0 +1,92 @@
+import os
+
+import time
+
+import torch
+import torchvision
+from torch import nn, optim
+from torchvision import transforms
+
+from research.PASCAL.P2005.net import GoogLeNet
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+WORK_DIR = '../../../../../data/PASCAL/P2005'
+NUM_EPOCHS = 20
+BATCH_SIZE = 16
+LEARNING_RATE = 1e-4
+NUM_CLASSES = 4
+
+MODEL_PATH = '../../../../models/pytorch/PASCAL/'
+MODEL_NAME = 'P2005.pth'
+
+# Create model
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(MODEL_PATH)
+
+transform = transforms.Compose([
+    transforms.Resize(96),  # 调整图片大小
+    transforms.RandomHorizontalFlip(),  # 几率随机旋转
+    transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # 归一化
+])
+
+
+# Load data
+train_dataset = torchvision.datasets.ImageFolder(root=WORK_DIR + '/' + 'train',
+                                                 transform=transform)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=BATCH_SIZE,
+                                           shuffle=True)
+
+
+def main():
+    print(f"Train numbers:{len(train_dataset)}")
+
+    model = GoogLeNet()
+    # cast
+    cast = nn.CrossEntropyLoss().to(device)
+    # Optimization
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=LEARNING_RATE,
+        weight_decay=1e-8)
+    step = 1
+    for epoch in range(1, NUM_EPOCHS + 1):
+        model.train()
+
+        # cal one epoch time
+        start = time.time()
+
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = cast(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print(f"Step [{step * BATCH_SIZE}/{NUM_EPOCHS * len(train_dataset)}], "
+                  f"Loss: {loss.item():.8f}.")
+            step += 1
+
+        # cal train one epoch time
+        end = time.time()
+        print(f"Epoch [{epoch}/{NUM_EPOCHS}], "
+              f"time: {end-start}!")
+
+        # Save the model checkpoint
+        torch.save(model, MODEL_PATH + MODEL_NAME)
+    print(f"Model save to {MODEL_PATH + MODEL_NAME}.")
+
+
+if __name__ == '__main__':
+    main()
