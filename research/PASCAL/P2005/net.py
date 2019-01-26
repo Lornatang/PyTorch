7097c325f3135027ae14b887aa0e9c87fc869e44
Lornatang <shiyipaisizuo@163.com>
2019-01-26 19:41:08 +0800

    Update net.py

--- research/PASCAL/P2005/net.py
+++ research/PASCAL/P2005/net.py
@@ -1,10 +1,10 @@
 import torch
-from torch import nn
+import torch.nn as nn
 
 NUM_CLASSES = 4
 
 
-class inception(nn.Module):
+class Inception(nn.Module):
     def __init__(self,
                  in_channels,
                  out_channels1_1,
@@ -13,7 +13,7 @@ class inception(nn.Module):
                  out_channels3_1,
                  out_channels3_5,
                  out_channels4_1):
-        super(inception, self).__init__()
+        super(Inception, self).__init__()
         # First line
         self.branch1x1 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels1_1, kernel_size=1, stride=1, padding=0),
@@ -59,7 +59,7 @@ class inception(nn.Module):
 
 
 class GoogLeNet(nn.Module):
-    """use google network(inception v3).
+    """use google network(Inception v3).
     input img size is 96 * 96"""
 
     def __init__(self, num_classes=NUM_CLASSES):
@@ -80,21 +80,21 @@ class GoogLeNet(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.block3 = nn.Sequential(
-            inception(192, 64, 96, 128, 16, 32, 32),
-            inception(256, 128, 128, 192, 32, 96, 64),
+            Inception(192, 64, 96, 128, 16, 32, 32),
+            Inception(256, 128, 128, 192, 32, 96, 64),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.block4 = nn.Sequential(
-            inception(480, 192, 96, 208, 16, 48, 64),
-            inception(512, 160, 112, 224, 24, 64, 64),
-            inception(512, 128, 128, 256, 24, 64, 64),
-            inception(512, 112, 144, 288, 32, 64, 64),
-            inception(528, 256, 160, 320, 32, 128, 128),
+            Inception(480, 192, 96, 208, 16, 48, 64),
+            Inception(512, 160, 112, 224, 24, 64, 64),
+            Inception(512, 128, 128, 256, 24, 64, 64),
+            Inception(512, 112, 144, 288, 32, 64, 64),
+            Inception(528, 256, 160, 320, 32, 128, 128),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.block5 = nn.Sequential(
-            inception(832, 256, 160, 320, 32, 128, 128),
-            inception(832, 384, 182, 384, 48, 128, 128),
+            Inception(832, 256, 160, 320, 32, 128, 128),
+            Inception(832, 384, 182, 384, 48, 128, 128),
             nn.AvgPool2d(kernel_size=2)
         )
         self.classifier = nn.Linear(9216, num_classes)
