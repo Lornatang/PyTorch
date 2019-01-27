9e1f5b1d076cfaafeb2295f8242f98226832189e
lorna assistant <41311334+Shiyipaisizuo@users.noreply.github.com>
2019-01-27 15:28:12 +0800

    update net writern

--- research/CIFAR/cifar10/net.py
+++ research/CIFAR/cifar10/net.py
@@ -4,107 +4,106 @@ from torch import nn
 NUM_CLASSES = 10
 
 
-class inception(nn.Module):
+class Inception(nn.Module):
     def __init__(self,
                  in_channels,
-                 out_channels1_1,
-                 out_channels2_1,
-                 out_channels2_3,
-                 out_channels3_1,
-                 out_channels3_5,
-                 out_channels4_1):
-        super(inception, self).__init__()
-        # First line
-        self.branch1x1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels1_1, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(out_channels1_1),
-            nn.ReLU(inplace=True)
+                 n1x1,
+                 n3x3red,
+                 n3x3,
+                 n5x5red,
+                 n5x5,
+                 out_channels):
+        super(Inception, self).__init__()
+        # 1x1 conv branch
+        self.b1 = nn.Sequential(
+            nn.Conv2d(in_channels, n1x1, kernel_size=1),
+            nn.BatchNorm2d(n1x1),
+            nn.ReLU(True)
         )
 
-        # Second line
-        self.branch3x3 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels2_1, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(out_channels2_1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels2_1, out_channels2_3, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels2_3),
-            nn.ReLU(inplace=True)
+        # 1x1 conv -> 3x3 conv branch
+        self.b2 = nn.Sequential(
+            nn.Conv2d(in_channels, n3x3red, kernel_size=1),
+            nn.BatchNorm2d(n3x3red),
+            nn.ReLU(True),
+            nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n3x3),
+            nn.ReLU(True)
         )
 
-        # Third line
-        self.branch5x5 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels3_1, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(out_channels3_1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels3_1, out_channels3_5, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(out_channels3_5),
-            nn.ReLU(inplace=True)
+        # 1x1 conv -> 5x5 conv branch
+        self.b3 = nn.Sequential(
+            nn.Conv2d(in_channels, n5x5red, kernel_size=1),
+            nn.BatchNorm2d(n5x5red),
+            nn.ReLU(True),
+            nn.Conv2d(n5x5red, n5x5, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n5x5),
+            nn.ReLU(True),
+            nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n5x5),
+            nn.ReLU(True)
         )
 
-        # Fourth line
-        self.branch_pool = nn.Sequential(
-            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_channels, out_channels4_1, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(out_channels4_1),
-            nn.ReLU(inplace=True)
+        # 3x3 pool -> 1x1 conv branch
+        self.b4 = nn.Sequential(
+            nn.MaxPool2d(3, stride=1, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True)
         )
 
     def forward(self, x):
-        f1 = self.branch1x1(x)
-        f2 = self.branch3x3(x)
-        f3 = self.branch5x5(x)
-        f4 = self.branch_pool(x)
-        out = torch.cat((f1, f2, f3, f4), dim=1)
-        return out
+        f1 = self.b1(x)
+        f2 = self.b2(x)
+        f3 = self.b3(x)
+        f4 = self.b4(x)
+        return torch.cat([f1, f2, f3, f4], 1)
 
 
 class GoogLeNet(nn.Module):
     """use google network(inception v3).
-    input img size is 96 * 96"""
+    input img size is 224 * 224"""
 
     def __init__(self, num_classes=NUM_CLASSES):
         super(GoogLeNet, self).__init__()
-        self.block1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        )
-        self.block2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 192, kernel_size=3, stride=1, padding=1),
+        self.pre_layers = nn.Sequential(
+            nn.Conv2d(3, 192, kernel_size=3, padding=1),
             nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2)
         )
-        self.block3 = nn.Sequential(
-            inception(192, 64, 96, 128, 16, 32, 32),
-            inception(256, 128, 128, 192, 32, 96, 64),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        )
-        self.block4 = nn.Sequential(
-            inception(480, 192, 96, 208, 16, 48, 64),
-            inception(512, 160, 112, 224, 24, 64, 64),
-            inception(512, 128, 128, 256, 24, 64, 64),
-            inception(512, 112, 144, 288, 32, 64, 64),
-            inception(528, 256, 160, 320, 32, 128, 128),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        )
-        self.block5 = nn.Sequential(
-            inception(832, 256, 160, 320, 32, 128, 128),
-            inception(832, 384, 182, 384, 48, 128, 128),
-            nn.AvgPool2d(kernel_size=2)
-        )
+
+        self.a3 = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a4 = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.b4 = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.c4 = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.d4 = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+
+        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+
         self.classifier = nn.Linear(1024, num_classes)
 
     def forward(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = x.view(x.size(0), -1)
-        out = self.classifier(x)
+        out = self.pre_layers(x)
+        out = self.a3(out)
+        out = self.b3(out)
+        out = self.maxpool(out)
+        out = self.a4(out)
+        out = self.b4(out)
+        out = self.c4(out)
+        out = self.d4(out)
+        out = self.e4(out)
+        out = self.maxpool(out)
+        out = self.a5(out)
+        out = self.b5(out)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
         return out
