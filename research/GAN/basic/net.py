354d6ecbab0941a10f660513c830d31c076049f5
Lornatang <shiyipaisizuo@163.com>
2019-01-29 21:49:36 +0800

    update

--- research/GAN/basic/net.py
+++ research/GAN/basic/net.py
@@ -1,22 +1,28 @@
 from torch import nn
 
-INPUT_SIZE = 32 * 32 * 3
-HIDDEN_SIZE = 1024
+IMAGE_SIZE = 28 * 28 * 3
+G_HIDDEN_SIZE = 1024
+D_HIDDEN_SIZE = 256
+
+NOISE = 100
 
 
 # Generator
 class Generator(nn.Module):
-    def __init__(self, input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, output_size=INPUT_SIZE):
+    def __init__(self, noise=NOISE):
         super(Generator, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
+            nn.Linear(noise, G_HIDDEN_SIZE),
             nn.ReLU(True)
         )
         self.layer2 = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size),
+            nn.Linear(G_HIDDEN_SIZE, G_HIDDEN_SIZE),
             nn.ReLU(True)
         )
-        self.layer3 = nn.Linear(hidden_size, output_size)
+        self.layer3 = nn.Sequential(
+            nn.Linear(G_HIDDEN_SIZE, IMAGE_SIZE),
+            nn.Tanh()
+        )
 
     def forward(self, x):
         x = self.layer1(x)
@@ -27,20 +33,17 @@ class Generator(nn.Module):
 
 # Discriminator
 class Discriminator(nn.Module):
-    def __init__(self, input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, output_size=INPUT_SIZE):
+    def __init__(self):
         super(Discriminator, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(True)
+            nn.Linear(IMAGE_SIZE, D_HIDDEN_SIZE),
+            nn.LeakyReLU(0.2, True)
         )
         self.layer2 = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(True)
+            nn.Linear(D_HIDDEN_SIZE, D_HIDDEN_SIZE),
+            nn.LeakyReLU(0.2, True)
         )
-        self.layer3 = nn.Sequential (
-            nn.Linear(hidden_size, output_size),
-            nn.ReLU(True)
-        )
+        self.layer3 = nn.Linear(D_HIDDEN_SIZE, 1)
 
     def forward(self, x):
         x = self.layer1(x)
