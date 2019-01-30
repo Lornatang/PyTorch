fe421135f1ce4853ce9942a8af55770d9622d1b1
Lornatang <shiyipaisizuo@163.com>
2019-01-30 08:08:55 +0800

    rename classifier img filr

--- research/MNIST/mnist/classifier_img.py
+++ research/MNIST/mnist/classifier_img.py
@@ -1,26 +1,35 @@
+import os
+
 import torch
 import torchvision
 from torchvision import transforms
 
+# from research.MNIST.mnist.net import Net
+
+WORK_DIR = '../../../../../data/MNIST/mnist'
 BATCH_SIZE = 1
 
-MODEL_PATH = '../../../models/pytorch/MNIST/'
+MODEL_PATH = '../../../../models/pytorch/MNIST/'
 MODEL_NAME = 'mnist.pth'
 
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Tensor image transforms to PIL image
+to_pil_image = transforms.ToPILImage()
 
-transform = transforms.Compose([
-    transforms.Resize(28),  # 将图像转化为224 * 224
-    transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # 归一化
-])
+label = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+# check file name is exist
+for dir_index in range(0, 10):
+    if not os.path.exists(WORK_DIR + '/' + 'test' + '/' + label[dir_index]):
+        os.makedirs(WORK_DIR + '/' + 'test' + '/' + label[dir_index])
+
+transform = transforms.ToTensor()
 
 # Load data
-test_dataset = torchvision.datasets.ImageFolder(root='test',
+test_dataset = torchvision.datasets.ImageFolder(root=WORK_DIR + '/' + 'test',
                                                 transform=transform)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
@@ -37,15 +46,18 @@ def main():
     else:
         model = torch.load(MODEL_PATH + MODEL_NAME, map_location='cpu')
 
-    model.eval()
-
-    for images, labels in test_loader:
+    index = 0
+    for images, _ in test_loader:
         # to GPU
         images = images.to(device)
         # print prediction
         outputs = model(images)
         # equal prediction and acc
         _, predicted = torch.max(outputs.data, 1)
+
+        img = to_pil_image(images[0])
+        img.save(str(WORK_DIR + '/' + 'test' + '/' + label[predicted]) + '/' + str(index) + '.jpg')
+        index += 1
 
 
 if __name__ == '__main__':
