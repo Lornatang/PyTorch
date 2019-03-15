4ed1114b0235ae4782472f07ecd842d956017bd1
Lornatang <shiyipaisizuo@163.com>
2019-03-16 00:14:25 +0800

    reindent

--- research/nn/lenet.py
+++ research/nn/lenet.py
@@ -23,14 +23,30 @@ MODEL_NAME = 'LeNet.pth'
 if not os.path.exists(MODEL_PATH):
   os.makedirs(MODEL_PATH)
 
+transform = transforms.Compose([
+  transforms.RandomCrop(36, padding=4),
+  transforms.RandomSizedCrop(32),
+  transforms.RandomHorizontalFlip(),
+  transforms.ToTensor(),
+  transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+
+# Load data
+train_dataset = torchvision.datasets.ImageFolder(root=WORK_DIR + '/' + 'train',
+                                                 transform=transform)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=BATCH_SIZE,
+                                           shuffle=True)
+
 
 class LeNet(nn.Module):
   """use myself network.
   input img size is 32 * 32
-  
+
   Args:
     num_classes: img classes.
-  
+
   """
   
   def __init__(self, num_classes=NUM_CLASSES):
@@ -62,23 +78,6 @@ class LeNet(nn.Module):
     return out
 
 
-transform = transforms.Compose([
-  transforms.RandomCrop(36, padding=4),
-  transforms.RandomSizedCrop(32),
-  transforms.RandomHorizontalFlip(),
-  transforms.ToTensor(),
-  transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-])
-
-# Load data
-train_dataset = torchvision.datasets.ImageFolder(root=WORK_DIR + '/' + 'train',
-                                                 transform=transform)
-
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=BATCH_SIZE,
-                                           shuffle=True)
-
-
 def main():
   print(f"Train numbers:{len(train_dataset)}")
   
@@ -94,32 +93,32 @@ def main():
   step = 1
   for epoch in range(1, NUM_EPOCHS + 1):
     model.train()
-  
+    
     # cal one epoch time
     start = time.time()
-  
+    
     for images, labels in train_loader:
       images = images.to(device)
       labels = labels.to(device)
-    
+      
       # Forward pass
       outputs = model(images)
       loss = cast(outputs, labels)
-    
+      
       # Backward and optimize
       optimizer.zero_grad()
       loss.backward()
       optimizer.step()
-    
+      
       print(f"Step [{step * BATCH_SIZE}/{NUM_EPOCHS * len(train_dataset)}], "
             f"Loss: {loss.item():.8f}.")
       step += 1
-  
+    
     # cal train one epoch time
     end = time.time()
     print(f"Epoch [{epoch}/{NUM_EPOCHS}], "
           f"time: {end - start} sec!")
-  
+    
     # Save the model checkpoint
     torch.save(model, MODEL_PATH + '/' + MODEL_NAME)
   print(f"Model save to {MODEL_PATH + '/' + MODEL_NAME}.")
