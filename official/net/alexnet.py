c3e9ba272f262c35b9a1b7a6fb62dfc9373d8cb9
Lornatang <shiyipaisizuo@163.com>
2019-03-31 23:55:37 +0800

    Update alexnet.py

--- official/net/alexnet.py
+++ official/net/alexnet.py
@@ -10,19 +10,50 @@ import torchvision.datasets as dset
 import torch.nn.functional as F
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', required=True, help='cifar10 | mnist | folder')
+parser.add_argument(
+  '--dataset',
+  required=True,
+  help='cifar10/100 | fmnist/mnist | folder')
 parser.add_argument('--dataroot', required=True, help='path to dataset')
-parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--batchSize', type=int, default=32, help='inputs batch size')
-parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the inputs image to network')
-parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.0001, help='learning rate, default=0.0001')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument(
+  '--workers',
+  type=int,
+  help='number of data loading workers',
+  default=2)
+parser.add_argument(
+  '--batchSize',
+  type=int,
+  default=32,
+  help='inputs batch size')
+parser.add_argument(
+  '--imageSize',
+  type=int,
+  default=32,
+  help='the height / width of the inputs image to network')
+parser.add_argument(
+  '--niter',
+  type=int,
+  default=25,
+  help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0001,
+                    help='learning rate, default=0.0001')
+parser.add_argument('--beta1', type=float, default=0.5,
+                    help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
-parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
-parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument(
+  '--ngpu',
+  type=int,
+  default=1,
+  help='number of GPUs to use')
+parser.add_argument(
+  '--outf',
+  default='.',
+  help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
-parser.add_argument('--model', required=True, help='training models or testing models')
+parser.add_argument(
+  '--model',
+  required=True,
+  help='training models or testing models')
 
 opt = parser.parse_args()
 print(opt)
@@ -41,49 +72,85 @@ torch.manual_seed(opt.manualSeed)
 if torch.cuda.is_available() and not opt.cuda:
   print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
-if opt.dataset in 'folder':
-  # folder dataset
-  dataset = dset.ImageFolder(root=opt.dataroot,
-                             transform=transforms.Compose([
-                               transforms.Resize(opt.imageSize),
-                               transforms.CenterCrop(opt.imageSize),
-                               transforms.RandomHorizontalFlip(),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                             ]))
-  nc = 3
-elif opt.dataset == 'cifar10':
-  dataset = dset.CIFAR10(root=opt.dataroot, download=True,
-                         transform=transforms.Compose([
-                           transforms.Resize(opt.imageSize),
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                         ]))
-  nc = 3
+transform = transforms.Compose([
+  transforms.Resize(opt.imageSize),
+  transforms.CenterCrop(opt.imageSize),
+  transforms.RandomHorizontalFlip(),
+  transforms.ToTensor(),
+  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
 
-elif opt.dataset == 'mnist':
-  dataset = dset.ImageFolder(root=opt.dataroot,
-                             transform=transforms.Compose([
-                               transforms.Resize(opt.imageSize),
-                               transforms.CenterCrop(opt.imageSize),
-                               transforms.RandomHorizontalFlip(),
-                               transforms.Grayscale(),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5,), (0.5,))
-                             ]))
-  nc = 1
+if opt.model == 'train':
+  if opt.dataset in 'folder':
+    # folder dataset
+    dataset = dset.ImageFolder(root=opt.dataroot,
+                               transform=transform)
+    nc = 3
+  elif opt.dataset == 'cifar10':
+    dataset = dset.CIFAR10(root=opt.dataroot,
+                           download=True,
+                           transform=transform)
+    nc = 3
+  elif opt.dataset == 'cifar100':
+    dataset = dset.CIFAR100(root=opt.dataroot,
+                            download=True,
+                            transform=transform)
+    nc = 3
+  elif opt.dataset == 'mnist':
+    dataset = dset.MNIST(root=opt.dataroot,
+                         download=True,
+                         transform=transform)
+    nc = 1
+  elif opt.dataset == 'fmnist':
+    dataset = dset.FashionMNIST(root=opt.dataroot,
+                                download=True,
+                                transform=transform)
+    nc = 1
+elif opt.model == 'test':
+  if opt.dataset in 'folder':
+    # folder dataset
+    dataset = dset.ImageFolder(root=opt.dataroot,
+                               transform=transform)
+    nc = 3
+  elif opt.dataset == 'cifar10':
+    dataset = dset.CIFAR10(root=opt.dataroot,
+                           download=True,
+                           train=False,
+                           transform=transform)
+    nc = 3
+  elif opt.dataset == 'cifar100':
+    dataset = dset.CIFAR100(root=opt.dataroot,
+                            download=True,
+                            train=False,
+                            transform=transform)
+    nc = 3
+  elif opt.dataset == 'mnist':
+    dataset = dset.MNIST(root=opt.dataroot,
+                         download=True,
+                         train=False,
+                         transform=transform)
+    nc = 1
+  elif opt.dataset == 'fmnist':
+    dataset = dset.FashionMNIST(root=opt.dataroot,
+                                download=True,
+                                train=False,
+                                transform=transform)
+    nc = 1
 
 assert dataset
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-                                         shuffle=True, num_workers=int(opt.workers))
-
+dataloader = torch.utils.data.DataLoader(
+  dataset,
+  batch_size=opt.batchSize,
+  shuffle=True,
+  num_workers=int(
+    opt.workers))
 
 device = torch.device("cuda:0" if opt.cuda else "cpu")
 ngpu = int(opt.ngpu)
 
 
 class AlexNet(nn.Module):
-  
+
   def __init__(self, ngpus):
     super(AlexNet, self).__init__()
     self.ngpu = ngpus
@@ -110,9 +177,9 @@ class AlexNet(nn.Module):
       nn.Dropout(),
       nn.Linear(4096, 4096),
       nn.ReLU(inplace=True),
-      nn.Linear(4096, 101),
+      nn.Linear(4096, 10),
     )
-  
+
   def forward(self, inputs):
     inputs = self.features(inputs)
     inputs = inputs.view(-1, 256 * 1 * 1)
@@ -122,7 +189,7 @@ class AlexNet(nn.Module):
 
 def train():
   print(f"Train numbers:{len(dataset)}")
-  
+
   # load model
   model = AlexNet(ngpu).to(device)
   # Optimization
@@ -131,7 +198,7 @@ def train():
     lr=opt.lr,
     betas=(opt.beta1, 0.999),
     weight_decay=1e-8)
-  
+
   for epoch in range(opt.niter):
     model.train()
     for batch_idx, (data, target) in enumerate(dataloader, 0):
@@ -143,17 +210,17 @@ def train():
       # Backward and update paras
       loss.backward()
       optimizer.step()
-      
+
       if batch_idx % 10 == 0:
         print(f"Train Epoch: {epoch} "
               f"[{batch_idx * len(data)}/{len(dataloader.dataset)} "
               f"({100. * batch_idx / len(dataloader):.0f}%)]\t"
               f"Loss: {loss.item():.6f}")
-    
+
     # Save the model checkpoint
     torch.save(model, f"{opt.outf}/AlexNet_epoch_{epoch + 1}.pth")
   print(f"Model save to '{opt.outf}'.")
-  
+
 
 def test():
   model = torch.load(f'{opt.outf}/AlexNet_epoch_{opt.niter}.pth')
@@ -164,17 +231,20 @@ def test():
     for data, target in dataloader:
       data, target = data.to(device), target.to(device)
       output = model(data)
-      test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-      pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+      # sum up batch loss
+      test_loss += F.nll_loss(output, target, reduction='sum').item()
+      # get the index of the max log-probability
+      pred = output.argmax(dim=1, keepdim=True)
       correct += pred.eq(target.view_as(pred)).sum().item()
-  
+
   test_loss /= len(dataloader.dataset)
-  
-  print(f"\nTest set: Average loss: {test_loss:.4f}, "
-        f"Accuracy: {correct}/{len(dataloader.dataset)} ({100. * correct / len(dataloader.dataset):.0f}%)\n")
+
+  print(
+    f"\nTest set: Average loss: {test_loss:.4f}, "
+    f"Accuracy: {correct}/{len(dataloader.dataset)} ({100. * correct / len(dataloader.dataset):.0f}%)\n")
 
 
 if opt.model == 'train':
   train()
-if opt.model == 'test':
+elif opt.model == 'test':
   test()
