a98045ecdb89a5eed466a73edb21db1d1b73c244
Lornatang <shiyipaisizuo@163.com>
2019-04-01 23:35:48 +0800

    Update download_img.py

--- tools/download_img.py
+++ tools/download_img.py
@@ -4,56 +4,58 @@ import requests
 import os
 
 
-os.makedirs('./data')
+if not os.path.isdir('./data'):
+  os.makedirs('./data')
 
 
 def get_page(key, page, n):
-    page = page * n
-    word = urllib.parse.quote(key, safe='/')
-    url_begin = "http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word="
-    url = url_begin + word + "&pn=" + str(page) + "&gsm=" + str(hex(page)) + "&ct=&ic=0&lm=-1&width=0&height=0"
-    return url
+  page = page * n
+  word = urllib.parse.quote(key, safe='/')
+  url_begin = "http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word="
+  url = url_begin + word + "&pn=" + \
+        str(page) + "&gsm=" + str(hex(page)) + "&ct=&ic=0&lm=-1&width=0&height=0"
+  return url
 
 
 def get_onepage_urls(onepageurl):
-    try:
-        html = requests.get(onepageurl).text
-    except Exception as e:
-        print(e)
-        pic_urls = []
-        return pic_urls
-    pic_urls = re.findall('"objURL":"(.*?)",', html, re.S)
+  try:
+    html = requests.get(onepageurl).text
+  except Exception as e:
+    print(e)
+    pic_urls = []
     return pic_urls
+  pic_urls = re.findall('"objURL":"(.*?)",', html, re.S)
+  return pic_urls
 
 
 def down_pic(pic_urls):
-    for i, pic_url in enumerate(pic_urls):
-        try:
-            pic = requests.get(pic_url, timeout=15)
-            string = 'data' + '/' + str(i + 1) + '.jpg'
-            with open(string, 'wb') as f:
-                f.write(pic.content)
-                print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
-        except Exception as e:
-            print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
-            print(e)
-            continue
+  for i, pic_url in enumerate(pic_urls):
+    try:
+      pic = requests.get(pic_url, timeout=15)
+      string = 'data' + '/' + str(i + 1) + '.jpg'
+      with open(string, 'wb') as f:
+        f.write(pic.content)
+        print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
+    except Exception as e:
+      print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
+      print(e)
+      continue
 
 
 if __name__ == '__main__':
-    keyword = '张杰'  # 关键词, 改为你想输入的词即可, 相当于在百度图片里搜索一样
-    page_begin = 0
-    page_number = 30
-    image_number = 8
-    all_pic_urls = []
-    while 1:
-        if page_begin > image_number:
-            break
-        print(f"第{page_begin+1}次请求数据.")
-        url = get_page(keyword, page_begin, page_number)
-        onepage_urls = get_onepage_urls(url)
-        page_begin += 1
+  keyword = '张杰高清壁纸'  # 关键词, 改为你想输入的词即可, 相当于在百度图片里搜索一样
+  page_begin = 0
+  page_number = 1000
+  image_number = 10
+  all_pic_urls = []
+  while True:
+    if page_begin > image_number:
+      break
+    print(f"第{page_begin+1}次请求数据.")
+    url = get_page(keyword, page_begin, page_number)
+    onepage_urls = get_onepage_urls(url)
+    page_begin += 1
 
-        all_pic_urls.extend(onepage_urls)
+    all_pic_urls.extend(onepage_urls)
 
-    down_pic(list(set(all_pic_urls)))
+  down_pic(list(set(all_pic_urls)))
