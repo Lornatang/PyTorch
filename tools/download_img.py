3e112a326a719790464bebd69bd96a5e27dfbe5a
Lornatang <shiyipaisizuo@163.com>
2019-04-01 23:47:54 +0800

    Update download_img.py

--- tools/download_img.py
+++ tools/download_img.py
@@ -1,61 +1,61 @@
 import re
-import urllib.parse
+import urllib.request
+
 import requests
-import os
-
-
-if not os.path.isdir('./data'):
-  os.makedirs('./data')
-
-
-def get_page(key, page, n):
-  page = page * n
-  word = urllib.parse.quote(key, safe='/')
-  url_begin = "http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word="
-  url = url_begin + word + "&pn=" + \
-        str(page) + "&gsm=" + str(hex(page)) + "&ct=&ic=0&lm=-1&width=0&height=0"
-  return url
 
 
 def get_onepage_urls(onepageurl):
-  try:
-    html = requests.get(onepageurl).text
-  except Exception as e:
-    print(e)
-    pic_urls = []
-    return pic_urls
-  pic_urls = re.findall('"objURL":"(.*?)",', html, re.S)
-  return pic_urls
+    """获取单个翻页的所有图片的urls+当前翻页的下一翻页的url"""
+    if not onepageurl:
+        print('已到最后一页, 结束')
+        return [], ''
+    try:
+        html = requests.get(onepageurl).text
+    except Exception as e:
+        print(e)
+        pic_urls = []
+        fanye_url = ''
+        return pic_urls, fanye_url
+    pic_urls = re.findall('"objURL":"(.*?)",', html, re.S)
+    fanye_urls = re.findall(
+        re.compile(r'<a href="(.*)" class="n">下一页</a>'),
+        html,
+        flags=0)
+    fanye_url = 'http://image.baidu.com' + fanye_urls[0] if fanye_urls else ''
+    return pic_urls, fanye_url
 
 
 def down_pic(pic_urls):
-  for i, pic_url in enumerate(pic_urls):
-    try:
-      pic = requests.get(pic_url, timeout=15)
-      string = 'data' + '/' + str(i + 1) + '.jpg'
-      with open(string, 'wb') as f:
-        f.write(pic.content)
-        print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
-    except Exception as e:
-      print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
-      print(e)
-      continue
+    """给出图片链接列表, 下载所有图片"""
+    for i, pic_url in enumerate(pic_urls):
+        try:
+            pic = requests.get(pic_url, timeout=15)
+            string = './data/' + str(i + 1) + '.jpg'
+            with open(string, 'wb') as f:
+                f.write(pic.content)
+                print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
+        except Exception as e:
+            print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
+            print(e)
+            continue
 
 
 if __name__ == '__main__':
-  keyword = '张杰高清壁纸'  # 关键词, 改为你想输入的词即可, 相当于在百度图片里搜索一样
-  page_begin = 0
-  page_number = 1000
-  image_number = 10
-  all_pic_urls = []
-  while True:
-    if page_begin > image_number:
-      break
-    print(f"第{page_begin+1}次请求数据.")
-    url = get_page(keyword, page_begin, page_number)
-    onepage_urls = get_onepage_urls(url)
-    page_begin += 1
-
+    keyword = '张杰高清壁纸'  # 关键词, 改为你想输入的词即可, 相当于在百度图片里搜索一样
+    url_init_first = r'http://image.baidu.com/search/flip?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=result' \
+                     r'&fr=&sf=1&fmq=1497491098685_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&isty' \
+                     r'pe=2&ie=utf-8&ctd=1497491098685%5E00_1519X735&word='
+    url_init = url_init_first + urllib.request.quote(keyword, safe='/')
+    all_pic_urls = []
+    onepage_urls, fanye_url = get_onepage_urls(url_init)
     all_pic_urls.extend(onepage_urls)
 
-  down_pic(list(set(all_pic_urls)))
+    fanye_count = 0  # 累计翻页数
+    while True:
+        onepage_urls, fanye_url = get_onepage_urls(fanye_url)
+        fanye_count += 1
+        print('第%s页' % fanye_count)
+        if fanye_url == '' and onepage_urls == []:
+            break
+        all_pic_urls.extend(onepage_urls)
+    down_pic(list(set(all_pic_urls)))
