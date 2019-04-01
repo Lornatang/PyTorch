3a90cbfab371672fa7bca71a68e442f9358cb19f
Lornatang <shiyipaisizuo@163.com>
2019-04-01 22:43:32 +0800

    clone from clone https://github.com/eriklindernoren/PyTorch-GAN.git

--- official/gan/dcgan/dcgan.py
+++ official/gan/dcgan/dcgan.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import random
+
+import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
@@ -15,7 +17,7 @@ parser.add_argument('--dataset', required=True, help='cifar10 | lsun | mnist |im
 parser.add_argument('--dataroot', required=True, help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='inputs batch size')
-parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the inputs image to network')
+parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the inputs image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
@@ -94,7 +96,10 @@ assert dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=int(opt.workers))
 
-device = torch.device("cuda:0" if opt.cuda else "cpu")
+if opt.cuda:
+  cuda = True if torch.cuda.is_available() else False
+else:
+  cuda = False
 ngpu = int(opt.ngpu)
 nz = int(opt.nz)
 ngf = int(opt.ngf)
@@ -112,30 +117,25 @@ def weights_init(m):
 
 
 class Generator(nn.Module):
-  def __init__(self, gpus):
+  def __init__(self):
     super(Generator, self).__init__()
-    self.ngpu = gpus
     self.main = nn.Sequential(
       # inputs is Z, going into a convolution
-      nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-      nn.BatchNorm2d(ngf * 8),
-      nn.ReLU(True),
-      # state size. (ngf*8) x 4 x 4
-      nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+      nn.ConvTranspose2d(nz, ngf * 4, 4, 1, 0, bias=False),
       nn.BatchNorm2d(ngf * 4),
       nn.ReLU(True),
-      # state size. (ngf*4) x 8 x 8
+      # state size. (ngf*4) x 4 x 4
       nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf * 2),
       nn.ReLU(True),
-      # state size. (ngf*2) x 16 x 16
+      # state size. (ngf*2) x 8 x 8
       nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf),
       nn.ReLU(True),
-      # state size. (ngf) x 32 x 32
+      # state size. (ngf) x 16 x 16
       nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
       nn.Tanh()
-      # state size. (nc) x 64 x 64
+      # state size. (nc) x 32 x 32
     )
   
   def forward(self, inputs):
@@ -146,35 +146,23 @@ class Generator(nn.Module):
     return outputs
 
 
-netG = Generator(ngpu).to(device)
-netG.apply(weights_init)
-if opt.netG != '':
-  torch.load(opt.netG)
-print(netG)
-
-
 class Discriminator(nn.Module):
-  def __init__(self, gpus):
+  def __init__(self):
     super(Discriminator, self).__init__()
-    self.ngpu = gpus
     self.main = nn.Sequential(
-      # inputs is (nc) x 64 x 64
+      # inputs is (nc) x 32 x 32
       nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf) x 32 x 32
+      # state size. (ndf) x 16 x 16
       nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ndf * 2),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*2) x 16 x 16
+      # state size. (ndf*2) x 8 x 8
       nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ndf * 4),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*4) x 8 x 8
-      nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 8),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*8) x 4 x 4
-      nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+      # state size. (ndf*4) x 4 x 4
+      nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
       nn.Sigmoid()
     )
   
@@ -187,21 +175,25 @@ class Discriminator(nn.Module):
     return outputs.view(-1, 1).squeeze(1)
 
 
-netD = Discriminator(ngpu).to(device)
-netD.apply(weights_init)
-if opt.netD != '':
-  torch.load(opt.netD)
-print(netD)
-
+# Loss function
 criterion = nn.BCELoss()
 
-fixed_noise = torch.randn(opt.batchSize, nz, 1, 1, device=device)
+# Initialize generator and discriminator
+Generator = Generator()
+Discriminator = Discriminator()
+
+if cuda:
+  criterion.cuda()
+  Discriminator.cuda()
+  Generator.cuda()
+
+fixed_noise = torch.randn(opt.batchSize, nz, 1, 1)
 real_label = 1
 fake_label = 0
 
 # setup optimizer
-optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerD = optim.Adam(Discriminator.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerG = optim.Adam(Generator.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 
 for epoch in range(opt.niter):
   for i, data in enumerate(dataloader, 0):
@@ -209,21 +201,21 @@ for epoch in range(opt.niter):
     # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
     ###########################
     # train with real
-    netD.zero_grad()
-    real_cpu = data[0].to(device)
+    Discriminator.zero_grad()
+    real_cpu = data[0]
     batch_size = real_cpu.size(0)
-    label = torch.full((batch_size,), real_label, device=device)
+    label = torch.full((batch_size,), real_label)
     
-    output = netD(real_cpu)
+    output = Discriminator(real_cpu)
     errD_real = criterion(output, label)
     errD_real.backward()
     D_x = output.mean().item()
     
     # train with fake
-    noise = torch.randn(batch_size, nz, 1, 1, device=device)
-    fake = netG(noise)
+    noise = torch.randn(batch_size, nz, 1, 1)
+    fake = Generator(noise)
     label.fill_(fake_label)
-    output = netD(fake.detach())
+    output = Discriminator(fake.detach())
     errD_fake = criterion(output, label)
     errD_fake.backward()
     D_G_z1 = output.mean().item()
@@ -233,26 +225,29 @@ for epoch in range(opt.niter):
     ############################
     # (2) Update G network: maximize log(D(G(z)))
     ###########################
-    netG.zero_grad()
+    Generator.zero_grad()
     label.fill_(real_label)  # fake labels are real for generator cost
-    output = netD(fake)
+    output = Discriminator(fake)
     errG = criterion(output, label)
     errG.backward()
     D_G_z2 = output.mean().item()
     optimizerG.step()
     
-    print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-          % (epoch, opt.niter, i, len(dataloader),
-             errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+    print(f"[{epoch}/{opt.niter}][{i}/{len(dataloader)}] "
+          f"Loss_D: {errD.item():.4f} "
+          f"Loss_G: {errG.item():.4f} "
+          f"D(x): {D_x:.4f} "
+          f"D(G(z)): {D_G_z1:.4f} / {D_G_z2:.4f}.")
+
     if i % 100 == 0:
       vutils.save_image(real_cpu,
-                        '%s/real_samples.png' % opt.outf,
+                        f"{opt.outf}/real_samples.png",
                         normalize=True)
-      fake = netG(fixed_noise)
+      fake = Generator(fixed_noise)
       vutils.save_image(fake.detach(),
-                        '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
+                        f"{opt.outf}/fake_samples_epoch_{epoch+1:03d}.png",
                         normalize=True)
   
   # do checkpointing
-  torch.save(netG, '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
-  torch.save(netD, '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+  torch.save(Generator, f"{opt.outf}/netG_epoch_{epoch+1}.pth")
+  torch.save(Discriminator, f"{opt.outf}/netD_epoch_{epoch+1}.pth")
