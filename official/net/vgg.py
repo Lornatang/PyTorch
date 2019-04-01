ebad4e6051d1a0a8dd743d339a30e03420410a69
Lornatang <shiyipaisizuo@163.com>
2019-04-01 22:46:32 +0800

    Update vgg.py

--- official/net/vgg.py
+++ official/net/vgg.py
@@ -16,7 +16,7 @@ parser.add_argument('--dataset', required=True, help='cifar-10/100 | fmnist/mnis
 parser.add_argument('--dataroot', required=True, help='path to dataset')
 parser.add_argument('--classes', type=int, required=True, help='classes of pictures')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
-parser.add_argument('--batchSize', type=int, default=64, help='inputs batch size')
+parser.add_argument('--batchSize', type=int, default=32, help='inputs batch size')
 parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the inputs image to network')
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0001, help='learning rate, default=0.0001')
