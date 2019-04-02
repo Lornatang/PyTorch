88c7599ec64b1f3b367290c2b1f8f9650e9644ac
Lornatang <shiyipaisizuo@163.com>
2019-04-02 21:24:48 +0800

    Update dcgan.py

--- official/gan/dcgan/dcgan.py
+++ official/gan/dcgan/dcgan.py
@@ -19,7 +19,7 @@ parser.add_argument('--imageSize', type=int, default=64, help='the height / widt
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=100, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=1000, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -146,13 +146,6 @@ class Generator(nn.Module):
     return outputs
 
 
-netG = Generator(ngpu).to(device)
-netG.apply(weights_init)
-if opt.netG != '':
-  netG = torch.load(opt.netG)
-print(netG)
-
-
 class Discriminator(nn.Module):
   def __init__(self, gpus):
     super(Discriminator, self).__init__()
@@ -189,9 +182,21 @@ class Discriminator(nn.Module):
 
 netD = Discriminator(ngpu).to(device)
 netD.apply(weights_init)
-if opt.netD != '':
-  netD = torch.load(opt.netD)
+
+netG = Generator(ngpu).to(device)
+netG.apply(weights_init)
+
+
+if opt.netD and opt.netG != '':
+  if torch.cuda.is_available():
+    netD = torch.load(opt.netD)
+    netG = torch.load(opt.netG)
+  else:
+    netD = torch.load(opt.netD, map_location='cpu')
+    netG = torch.load(opt.netG, map_location='cpu')
+
 print(netD)
+print(netG)
 
 criterion = nn.BCELoss()
 
@@ -244,7 +249,7 @@ def main():
       optimizerG.step()
 
       print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-            % (epoch, opt.niter, i, len(dataloader),
+            % (epoch+1, opt.niter, i, len(dataloader),
                errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
       if i % 100 == 0:
         vutils.save_image(real_cpu,
