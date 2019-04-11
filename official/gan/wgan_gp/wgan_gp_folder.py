44ce718b4e53a09335ef25791363185642e8a2dd
Lornatang <shiyipaisizuo@163.com>
2019-04-12 07:45:51 +0800

    Update wgan_gp_folder.py

--- official/gan/wgan_gp/wgan_gp_folder.py
+++ official/gan/wgan_gp/wgan_gp_folder.py
@@ -103,21 +103,25 @@ class Generator(nn.Module):
     self.ngpu = gpus
     self.main = nn.Sequential(
       # inputs is Z, going into a convolution
-      nn.ConvTranspose2d(nz, ngf * 4, 4, 1, 0, bias=False),
-      nn.BatchNorm2d(ngf * 4),
+      nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+      nn.BatchNorm2d(ngf * 8),
       nn.ReLU(True),
       # state size. (ngf*8) x 4 x 4
+      nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(ngf * 4),
+      nn.ReLU(True),
+      # state size. (ngf*4) x 8 x 8
       nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf * 2),
       nn.ReLU(True),
-      # state size. (ngf*4) x 8 x 8
+      # state size. (ngf*2) x 16 x 16
       nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf),
       nn.ReLU(True),
-      # state size. (ngf*2) x 16 x 16
-      nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
-      nn.Tanh(),
       # state size. (ngf) x 32 x 32
+      nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+      nn.Tanh()
+      # state size. (nc) x 64 x 64
     )
 
   def forward(self, inputs):
@@ -143,19 +147,24 @@ class Discriminator(nn.Module):
     super(Discriminator, self).__init__()
     self.ngpu = gpus
     self.main = nn.Sequential(
-      # inputs is (nc) x 32 x 32
+      # inputs is (nc) x 64 x 64
       nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf) x 16 x 16
+      # state size. (ndf) x 32 x 32
       nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ndf * 2),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*2) x 8 x 8
+      # state size. (ndf*2) x 16 x 16
       nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ndf * 4),
       nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*4) x 4 x 4
-      nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
+      # state size. (ndf*4) x 8 x 8
+      nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(ndf * 8),
+      nn.LeakyReLU(0.2, inplace=True),
+      # state size. (ndf*8) x 4 x 4
+      nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+      nn.Sigmoid()
     )
 
   def forward(self, inputs):
