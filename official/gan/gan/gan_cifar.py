0d9d3a06c3ce63bb478effe3b9371bb010c183b9
Lornatang <shiyipaisizuo@163.com>
2019-04-14 17:41:19 +0800

    Update gan_cifar.py

--- official/gan/gan/gan_cifar.py
+++ official/gan/gan/gan_cifar.py
@@ -18,7 +18,7 @@ parser.add_argument('--workers', type=int, help='number of data loading workers'
 parser.add_argument('--batchSize', type=int, default=64, help='inputs batch size')
 parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the inputs image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
-parser.add_argument('--niter', type=int, default=200, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -173,7 +173,7 @@ def main():
       netG.zero_grad()
 
       # Sample noise as generator input
-      noise = torch.randn(real_imgs.size(0), nz)
+      noise = torch.randn(real_imgs.size(0), nz, device=device)
 
       # Generate a batch of images
       fake_imgs = netG(noise)
