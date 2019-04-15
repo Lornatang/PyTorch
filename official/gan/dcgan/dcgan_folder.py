e6eaa41ce0fa4bd55b82614d800f712b118a3baa
Lornatang <shiyipaisizuo@163.com>
2019-04-15 10:57:00 +0800

    Update dcgan_folder.py

--- official/gan/dcgan/dcgan_folder.py
+++ official/gan/dcgan/dcgan_folder.py
@@ -84,13 +84,13 @@ nc = 3
 
 
 # custom weights initialization called on netG and netD
-def weights_init(m):
+def weights_init_normal(m):
   classname = m.__class__.__name__
-  if classname.find('Conv') != -1:
-    m.weight.data.normal_(0.0, 0.02)
-  elif classname.find('BatchNorm') != -1:
-    m.weight.data.normal_(1.0, 0.02)
-    m.bias.data.fill_(0)
+  if classname.find("Conv") != -1:
+    torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+  elif classname.find("BatchNorm") != -1:
+    torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+    torch.nn.init.constant_(m.bias.data, 0.0)
 
 
 class Generator(nn.Module):
