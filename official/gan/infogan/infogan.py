da581ba740e785168c2a3d8e8b514dd52efd5bea
Lornatang <shiyipaisizuo@163.com>
2019-04-15 14:02:04 +0800

    Update infogan.py

--- official/gan/infogan/infogan.py
+++ official/gan/infogan/infogan.py
@@ -34,6 +34,19 @@ print(opt)
 
 cuda = True if torch.cuda.is_available() else False
 
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "/tmp",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=opt.batch_size,
+    shuffle=True,
+)
+
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -141,21 +154,6 @@ if cuda:
 # Initialize weights
 netG.apply(weights_init_normal)
 netD.apply(weights_init_normal)
-
-# Configure data loader
-os.makedirs("/tmp", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "/tmp",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
 
 # Optimizers
 optimizerG = torch.optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
@@ -284,9 +282,9 @@ for epoch in range(opt.n_epochs):
 
         print(f"[Epoch {epoch}/{opt.n_epochs}] "
               f"[Batch {i}/{len(dataloader)}] "
-              f"[D loss: {errD.item()}] "
-              f"[G loss: {errG.item()}] "
-              f"[info loss: {errInfo.item()}]")
+              f"[D loss: {errD.item():.4f}] "
+              f"[G loss: {errG.item():.4f}] "
+              f"[info loss: {errInfo.item():.4f}]")
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
             sample_image(n_row=10, batches_done=batches_done)
