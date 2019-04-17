9a680c4eb6ad99285739b500b993d9f57a66f9ed
Lornatang <shiyipaisizuo@163.com>
2019-04-17 16:31:43 +0800

    Update wgan_gp_folder.py

--- official/gan/wgan_gp/wgan_gp_folder.py
+++ official/gan/wgan_gp/wgan_gp_folder.py
@@ -152,19 +152,15 @@ class Discriminator(nn.Module):
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf) x 32 x 32
       nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 2),
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf*2) x 16 x 16
       nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 4),
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf*4) x 8 x 8
       nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 8),
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf*8) x 4 x 4
       nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-      nn.Sigmoid()
     )
 
   def forward(self, inputs):
