8884e1f6c60330caee6cd39fa76ceb5c2117ec64
Lornatang <shiyipaisizuo@163.com>
2019-04-17 16:31:40 +0800

    Update wgan_gp_cifar.py

--- official/gan/wgan_gp/wgan_gp_cifar.py
+++ official/gan/wgan_gp/wgan_gp_cifar.py
@@ -142,15 +142,12 @@ class Discriminator(nn.Module):
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf) x 16 x 16
       nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 2),
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf*2) x 8 x 8
       nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 4),
       nn.LeakyReLU(0.2, inplace=True),
       # state size. (ndf*4) x 4 x 4
       nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
-      nn.Sigmoid()
     )
 
   def forward(self, inputs):
